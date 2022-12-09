0c96825cdfee6024efac1a2445aa965235875620
armandoBringas <armando.bringas@gmail.com>
2022-12-09 17:01:48 -0600

    Code refactor

--- reverse_hex_nibble/reverse_hex_nibble.c
+++ reverse_hex_nibble/reverse_hex_nibble.c
@@ -6,40 +6,35 @@
 
 #include <stdio.h>
 
-int reverse_hex_nibble(int hex_input);
+void reverse_hex_nibble(int *hex_input);
 
 int main(void)
 {
-    /* Testing variables */
+    /* Testing nibbles */
     int input_1 = 0x12;
     int input_2 = 0x85;
-
-    /* Print original variables */
     printf("Input nibble 1: 0x%x\n", input_1);
     printf("Input nibble 2; 0x%x\n", input_2);
 
-    /* Swap function calling */
-    int inverted_input1 = 0;
-    int inverted_input2 = 0;
-    inverted_input1 = reverse_hex_nibble(input_1);
-    inverted_input2 = reverse_hex_nibble(input_2);
-
-    /* Print swapped numbers*/
-    printf("Input nibble 1: 0x%x\n", inverted_input1);
-    printf("Input nibble 2: 0x%x\n", inverted_input2);
+    /* Invert nibbles */
+    reverse_hex_nibble(&input_1);
+    reverse_hex_nibble(&input_2);
+    printf("Inverted nibble 1: 0x%x\n", input_1);
+    printf("Inverted nibble 2: 0x%x\n", input_2);
 
     return 0;
 }
 
-int reverse_hex_nibble(int hex_input)
+
+void reverse_hex_nibble(int *hex_input_pntr)
 {
     unsigned int rev_num = 0;
-    while (hex_input)
+    while (*hex_input_pntr)
     {
         rev_num <<= 4;
-        rev_num |= hex_input & 0xF;
-        hex_input >>= 4;
+        rev_num |= *hex_input_pntr & 0xF;
+        *hex_input_pntr >>= 4;
     }
 
-  return rev_num;
+    *hex_input_pntr = rev_num;
 }
