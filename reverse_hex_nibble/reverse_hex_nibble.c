f81633f24e58a5595d4583585488c7cc3c40cde1
Armando Bringas <armando.bringas@gmail.com>
2022-12-12 19:28:15 -0600

    Changes in code to reference based solution and debugging function

--- reverse_hex_nibble/reverse_hex_nibble.c
+++ reverse_hex_nibble/reverse_hex_nibble.c
@@ -2,6 +2,7 @@
  * Armando Bringas
  * 12/9/2022
  * Reverse a hex nibble
+ * Solution based on: https://www.codeproject.com/Questions/738421/Logic-for-reverse-an-integer-byte-by-byte
  */
 
 #include <stdio.h>
@@ -29,12 +30,37 @@ int main(void)
 void reverse_hex_nibble(int *hex_input_pntr)
 {
     unsigned int rev_num = 0;
+
     while (*hex_input_pntr)
     {
         rev_num <<= 4;
         rev_num |= *hex_input_pntr & 0xF;
-        *hex_input_pntr >>= 4;
+        *hex_input_pntr >>= 4;        
     }
 
     *hex_input_pntr = rev_num;
 }
+
+/*
+void reverse_hex_nibble(int *hex_input_pntr)
+{
+    unsigned int rev_num = 0;
+    
+    printf("--------------------\n");
+    while (*hex_input_pntr)
+    {
+        printf("rev_num: 0x%x\n", rev_num);
+        printf("hex_input_pntr: 0x%x\n", *hex_input_pntr);
+        rev_num <<= 4;
+        printf("rev_num <<= 4: 0x%x\n", rev_num);
+        rev_num |= *hex_input_pntr & 0xF;
+        printf("rev_num |= *hex_input_pntr & 0xF: 0x%x\n", rev_num);
+        *hex_input_pntr >>= 4;
+        printf("*hex_input_pntr >>= 4: 0x%x\n", rev_num);
+        printf("--------------------\n");
+    }
+    printf("--------------------\n");
+
+    *hex_input_pntr = rev_num;
+}
+*/
